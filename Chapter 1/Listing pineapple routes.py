700cef63829a4e911e32ce3d73ec7456a0fa73ee
RuefulMage <feonorabd2000@gmail.com>
2020-10-30 22:19:44 +0300

    Listing pineapple routes solution added

---
+++ Chapter 1/Listing pineapple routes.py
@@ -0,0 +1,15 @@
+portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
+
+
+def permutations(route, ports):
+    if len(route) == 5:
+        print(' '.join([portnames[i] for i in route]))
+    else:
+        for i in ports:
+            new_port_list = ports.copy()
+            new_route_list = route.copy()
+            new_route_list.append(i)
+            new_port_list.remove(i)
+            permutations(new_route_list, new_port_list)
+
+permutations([0], list(range(1, len(portnames))))
